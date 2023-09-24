0e1edb1fb1eb30a087a370f8fdf7d5bcd9e6cb4e
AJ <ajecoder@gmail.com>
2023-09-24 17:26:45 -0400

    updated wall follower

--- warmup_project/warmup_project/wall_follower.py
+++ warmup_project/warmup_project/wall_follower.py
@@ -16,52 +16,61 @@ class WallFollower(Node):
         self.wheelOutput = [0.5, 0] #[linear angular]. Linear: positive is forward, negative is backwards. Angular: positive is counterclockwise, negative is clockwise.
         self.followingLeft = True
 
-    def ChooseSide(self):
-        ''' Returns which side the neato is probably following a wall on. We assume that if the wall is on the left, measurements from the right will be further away than the left.'''
-        self.followingLeft = True
-        #self.followingLeft= LaserScan[d180] < LaserScan[d0] #will be true if the 180-degree left hand size is closer.
-        
-
     def OrientToWall(self):
         """ outputs the wheel velocities needed to make the robot get closer to following the wall."""
-        pass
-        # if FollowingLeft:
-        #     if ahead > behind: # AKA 45 and d135
-        #         turnAmount = ahead/behind
-        #     elif ahead < behind:
-        #         turnAmount = -1*behind/ahead
-        #     else:
-        #         turnAmount = 0
-        # else: #following a wall to the right
-        #     if ahead > behind: #and here, AKA 225 and d315
-        #         turnAmount = -ahead/behind
-        #     elif ahead < behind:
-        #         turnAmount = behind/ahead
-        #     else:
-        #         turnAmount = 0
-        # return turnAmount
+        turnAmount = 0
+        distance = self.scanValues
+        if self.followingLeft == True:
+            #comparing 45 and 135 (front and back)
+            if distance[2] > distance[3]:
+                turnAmount = distance[2]/distance[3]
+            elif distance[2] < distance[3]:
+                turnAmount = -1*(distance[3]/distance[2])
+            else:
+                turnAmount = 0
+        else:
+            #comparing 315 and 225
+            if distance[5] > distance[4]:
+                turnAmount = -1*(distance[5]/distance[4])
+            elif distance[5] < distance[4]:
+                turnAmount = distance[4]/distance[5]
+            else:
+                turnAmount = 0
+        return turnAmount
+
+    def ChooseSide(self):
+        ''' Returns which side the neato is probably following a wall on. We assume that if the wall is on the left, measurements from the right will be further away than the left.'''
+        if self.scanValues == []:
+            pass #handle invalid values in important angles (zero)
+        if self.scanValues[0] >= self.scanValues[1]:
+            self.followingLeft = True #will be true if the 180-degree left hand size is closer.
+        else:
+            self.followingLeft = False
 
     def readLidar(self, msg):
         angles = [90, 180, 45, 135, 225, 315]
-        print(angles)
         self.scanValues = [] # Should give an empty list for appending
         print(msg.ranges)
-        #if msg.ranges.includes(0):
-        #    pass # TODO: FIRST: add print statements and make the node runnable. Get ride of pseudocode and just make it run. See where the data tyeps print.
+        for value in angles:
+            self.scanValues.append(msg.ranges[value]) #Should leave us with a 6-element list
+
+        if self.scanValues.includes(0):
+            self.scanValues = []
+        return self.ChooseSide()
+
+        # TODO: FIRST: add print statements and make the node runnable. Get ride of pseudocode and just make it run. See where the data types print.
         # SECOND: GET LIDAR SCANS THAT ARE GOOD, which means they don't have 0s in the 45 etc ranges that we care about.
         # THIRD, add a proportional multiplier to the output of the angular output commands. Test the neato.
-        # else:
-        #     for value in angles:
-        #         self.scanValues.append(msg.ranges[value]) #Should leave us with a 6-element list
 
     def updateVelocity(self):
         vels = Twist()
         vels.linear.x = self.wheelOutput[0]
         vels.angular.z = self.wheelOutput[1]
         self.vel_pub.publish(vels)
+
     def runLoop(self):
-        self.FollowingLeft = ChooseSide(LaserScan)
-        self.wheelOutput[0] = OrientToWall(LaserScan, FollowingLeft)
+        self.FollowingLeft = self.readLidar(LaserScan)
+        self.wheelOutput[0] = self.OrientToWall(LaserScan)
         self.setVelocity() #Set values for the twist object
     
 def main(args=None):
